11e3d0e9df42724bf504339349038397f2dfbdbe
naeem majumder <naeemmajumder00@gmail.com>
2026-04-18 23:35:10 +0600

    mongoose problem

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
     app.listen(5000, () => console.log('Server running'));
  */
 
+const dns = require("dns");
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
